556c1bd5c633583a742fc7269cc20429dd4986a7
Tzixuan <0171123334@mail.imu.edu.cn>
2019-10-19 22:18:19 +0800

    BaldTian-Csharp-SumOfDices

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace HW2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(17, 8);
+            this.chart1.Name = "chart1";
+            series3.BackImageAlignment = System.Windows.Forms.DataVisualization.Charting.ChartImageAlignmentStyle.Center;
+            series3.ChartArea = "ChartArea1";
+            series3.IsXValueIndexed = true;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            series3.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series3.YValuesPerPoint = 12;
+            series3.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(581, 706);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // chart2
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart2.Legends.Add(legend4);
+            this.chart2.Location = new System.Drawing.Point(634, 12);
+            this.chart2.Name = "chart2";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(797, 702);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(179, 687);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "两数之和统计图";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(909, 687);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 27);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "两数之和大致分布图";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1322, 767);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "色子和最大可能性";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;//添加chart的命名空间
+namespace HW2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitChart();
+        }
+        Random rnd = new Random();
+        int temp1, temp2,temp;
+        public void InitChart()//chart初始化
+        {
+            Series series = chart1.Series[0];
+            Series series2 = chart2.Series[0];
+            series.BorderWidth = 1;//柱状图 两数之和统计
+            ChartArea chartarea = chart1.ChartAreas[0];
+            chartarea.AxisX.Minimum = 2;
+            chartarea.AxisX.Maximum = 12;
+            chartarea.AxisY.Minimum = 0;
+            chartarea.AxisY.Maximum = 1000;
+            chartarea.AxisX.Interval = 1;
+
+            series2.BorderWidth = 1;//点状图 两数之和分布图
+            ChartArea chartarea2 = chart2.ChartAreas[0];
+            chartarea2.AxisX.Minimum = 0;
+            chartarea2.AxisX.Maximum = 500;
+            chartarea2.AxisY.Minimum = 2;
+            chartarea2.AxisY.Maximum = 12;
+            chartarea2.AxisY.Interval = 1;
+
+        }
+        Queue<int> Q1 = new Queue<int>();
+        int[] a=new int[13];
+        //a[13]={0};
+        public void Draw()
+        {
+            this.chart1.Series[0].Points.Clear();
+            temp1 = rnd.Next(1, 7);//产生1-6的随机数
+            temp2 = rnd.Next(1, 7);
+            temp = temp1 + temp2;//色子的和
+            for (int i = 2; i < 13; i++)
+            {
+                if (temp == i)//和计数
+                    a[i]++;
+                if (a[i] == 1000)//如果有一个达到1000，则停止
+                {
+                    timer1.Stop();
+                    this.Text = "投掷两个色子，其和的最大可能性为"+i;
+                }
+            }
+            for (int j = 1; j < 13; j++)//添加点
+                chart1.Series[0].Points.AddY(a[j]);
+           /* Q1.Enqueue(temp);
+            if (Q1.Count > 2)
+                Q1.Dequeue();*/
+            /*for (int j = 0; j < 1; j++)*/
+                chart2.Series[0].Points.AddY(temp);
+
+        }
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            Draw();
+        }
+    }
+}
